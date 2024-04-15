fc467f74a7deb6c0e241ee26575aca070c9f8689
santiagopereyraecheverria <141652797+santiagopereyraecheverria@users.noreply.github.com>
2024-04-15 11:51:09 -0300

    Ej 2, parte 1.cs

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,9 +7,11 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            // No se está utilizando el identificador único para la cita, lo que podría causar problemas en la gestión y seguimiento de las citas.
             string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
             Console.WriteLine(appointmentResult);
 
+            // Al pasar cadenas vacías para el ID y el nombre del médico, no detecta estos errores, lo que podría resultar en citas incorrectas o incompletas.
             string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
             Console.WriteLine(appointmentResult2);
         }
