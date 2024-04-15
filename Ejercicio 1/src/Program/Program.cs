98e4404c2abe77850d31c08c98e839855c2ff534
santiagopereyraecheverria <141652797+santiagopereyraecheverria@users.noreply.github.com>
2024-04-15 11:34:27 -0300

    Update Program (ej 1, parte 2).cs

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -1,17 +1,18 @@
-﻿using System;
+using System;
 
 namespace Ucu.Poo.Expert
 {
-
     class Program
     {
         static void Main()
         {
-            Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
-            Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            LibraryInventory inventory = new LibraryInventory();
 
+            Book book1 = new Book("Design Patterns", "Erich Gamma & Others", "001-034");
+            Book book2 = new Book("Pro C#", "Troelsen", "001-035");
+
+            inventory.ShelveBook(book1, "A", "7");
+            inventory.ShelveBook(book2, "B", "3");
         }
     }
 }
