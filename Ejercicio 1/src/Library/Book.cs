e7cd225f8a63041c4b195963cb3ab47cb7aab818
santiagopereyraecheverria <141652797+santiagopereyraecheverria@users.noreply.github.com>
2024-04-15 11:28:19 -0300

    Update Book (ej 1, parte 2).cs

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -1,3 +1,5 @@
+// Book.cs
+
 using System;
 
 namespace Ucu.Poo.Expert
@@ -7,20 +9,12 @@ namespace Ucu.Poo.Expert
         public string Title { get; }
         public string Author { get; }
         public string Code { get; }
-        public string LibrarySector { get; set; } // Esta propiedad parece pertenecer a una responsabilidad diferente
-        public string LibraryShelve { get; set; } // Esta propiedad parece pertenecer a una responsabilidad diferente
 
         public Book(String title, String author, String code)
         {
             this.Title = title;
             this.Author = author;
             this.Code = code;
-        }
-
-        public void ShelveBook(String sector, String shelve) // Este método también parece pertenecer a una responsabilidad diferente
-        {
-            this.LibrarySector = sector;
-            this.LibraryShelve = shelve;
         }
     }
 }
