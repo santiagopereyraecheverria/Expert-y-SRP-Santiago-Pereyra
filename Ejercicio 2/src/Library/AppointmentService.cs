1bebc7a4444cf8ac7c0f0d77c4d3010bf522f268
santiagopereyraecheverria <141652797+santiagopereyraecheverria@users.noreply.github.com>
2024-04-15 11:48:16 -0300

    Ej 2 parte 1.cs

--- Ejercicio 2/src/Library/AppointmentService.cs
+++ Ejercicio 2/src/Library/AppointmentService.cs
@@ -5,16 +5,21 @@ namespace Library
 {
     public class AppointmentService
     {
+        // Método sobrecargado para crear una cita. Esta sobrecarga podría ser confusa y complicar el mantenimiento.
         public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
         {
             StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
             Boolean isValid = true;
+
+            // Patrón Validación de datos: La validación de datos debería realizarse en una clase separada para seguir el principio SRP.
 
             if (string.IsNullOrEmpty(name))
             {
                 stringBuilder.Append("Unable to schedule appointment, 'name' is required\n");
                 isValid = false;
             }
+
+            // Repetición de lógica de validación en cada atributo. Esto podría simplificarse utilizando un enfoque más genérico.
 
             if (string.IsNullOrEmpty(id))
             {
@@ -34,13 +39,13 @@ namespace Library
                 isValid = false;
             }
 
-
             if (string.IsNullOrEmpty(doctorName))
             {
                 stringBuilder.Append("Unable to schedule appointment, 'doctor name' is required\n");
                 isValid = false;
             }
 
+            // La lógica de creación de citas y la lógica de validación deberían estar separadas para facilitar el mantenimiento y la extensión del código.
             if (isValid)
             {
                 stringBuilder.Append("Appoinment scheduled");
